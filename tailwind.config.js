ebddad96c60c50f6f9d12cf60904386c6585c4c6
Rafli Ramadhan <rafliramdhn@gmail.com>
2022-09-09 15:46:43 +0700

    fix build problem

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@ module.exports = {
   purge: [
     './src/**/*.html',
     './src/**/*.js',
+    './src/**/*.vue',
+    './src/**/*.jsx',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
